297ff9bf1ebbcca479c6b71e2a2f8c876501a4b1
zhangliang <914342085@qq.com>
2018-09-20 21:35:39 +0800

    node.js 生成id：增加uuid v4

--- NodeJS/GenID/genid.js
+++ NodeJS/GenID/genid.js
@@ -1,15 +1,28 @@
 const shortid = require('shortid');
-const uuid = require('uuid/v4');
+const uuid = require('uuid/v1');
+const uuid4 = require('uuid/v4');
 
 let id;
 
+// 普通的短id
 for (let i = 0; i < 10; i++) {
     id = shortid.generate();
     console.log(id, id.length);
 }
+
+// uuid v1是主要基于时间生成的，可以看出连续生成的id非常接近
+console.log('\n\nuuid v1');
+
 
 for (let i = 0; i < 10; i++) {
     id = uuid();
     console.log(id, id.length);
 }
 
+// uuid v4随机性比较大，相邻的两个id相差非常大
+console.log('\n\nuuid v4');
+
+for (let i = 0; i < 10; i++) {
+    id = uuid4();
+    console.log(id, id.length);
+}
