257df99d8163329581d5f709820d9518e725f569
zhangliang <914342085@qq.com>
2018-09-24 09:28:18 +0800

    node.js 1. 增加权限页面的是否登录判定 2. 登录后跳转到之前的页面

--- NodeJS/testExpress/routes/index.js
+++ NodeJS/testExpress/routes/index.js
@@ -15,6 +15,7 @@ passport.use(new LocalStrategy(function(username, password, done) {
 
 //将用户信息的一部分序列化后存在session中。当然也可以把所用用户信息全存在session中，那么反序列化时也要做相应改动
 //保存id的好处是，当用户信息有改动时，passport得到的用户信息可以实时改动；坏处是登录后每个页面都要查数据库（假如反序列化中查了数据库）。
+//可以考虑session里面只放id，但用户信息放在redis里面
 passport.serializeUser(function(user, done) {
   done(null, user.id);
 })
@@ -33,7 +34,13 @@ router.get('/', function(req, res, next) {
 router.get('/login',
   passport.authenticate('local'),
   function(req, res) {  //验证成功后，会把用户信息放在req.user中，通过回调函数使用； 验证失败，返回401错误，不执行回调
-    res.send('登录成功:' + JSON.stringify(req.user));
+    if (req.session.returnTo) {
+      let r = req.session.returnTo;
+      delete req.session.returnTo;      //经测试这句必须放在redirect之前才能真正删除session中的returnTo变量
+      res.redirect(r);
+    } else {
+      res.send('登录成功:' + JSON.stringify(req.user));
+    }
   }
 )
 
@@ -64,5 +71,14 @@ router.get('/logout', function(req, res, next){
   }
 })
 
+
+
+//通过connect-ensure-login模块可以把未登录时想访问的页面放入session.returnTo中，登录之后可通过这个返回之前的页面。
+router.get('/settings',
+  require('connect-ensure-login').ensureLoggedIn('/login'), //如果没有登录，则跳转到/login页面
+  function(req, res) {
+    res.send("设置页面：" + JSON.stringify(req.user));
+  }
+)
 
 module.exports = router;
