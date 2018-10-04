d4c86a3ff63eec034c53552cd269c92d18278331
zhangliang <914342085@qq.com>
2018-10-04 20:06:41 +0800

    bootstrap  增加下拉菜单

--- Bootstrap/navigation/nav1.js
+++ Bootstrap/navigation/nav1.js
@@ -1,10 +1,11 @@
 $(function(){
     console.log("OKK");
 
-    $(".navbar-nav li a").click(function(){
+    $(".navbar-nav li:not(.dropdown) a").click(function(){
         console.log("add");
         console.log($(this)[0]);
         $(".navbar-nav li").removeClass("active");
         $(this).parent().addClass("active");
     })
+
 });
