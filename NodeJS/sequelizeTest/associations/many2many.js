554e6df51d7d0b796cb506aa9108f3f0f2393bc5
zhangliang <914342085@qq.com>
2018-10-23 19:20:22 +0800

    node.js sequelize 修改多对多关系

--- NodeJS/sequelizeTest/associations/many2many.js
+++ NodeJS/sequelizeTest/associations/many2many.js
@@ -13,30 +13,27 @@ const Role = sequelize.define('role', {
     name: Sequelize.STRING
 });
 
-User.belongsToMany(Role, {through: 'userRole'});
+User.belongsToMany(Role, {through: 'userRole'});    //必须添加through,表示中间表的名称
+Role.belongsToMany(User, {through: 'userRole'});
 
-sequelize.sync({force: true}).then(function(){
+sequelize.sync({force: true}).then(function(){  //创建所有表
     console.log("创建所有表成功！");
 }).then(function(){
-    
+    return User.create({name:'zhang'}); //创建用户
+}).then(function(user){
+    return Role.create({name:'admin'}).then(function(role1){    //创建角色1
+        return [user, role1];
+    }); 
+}).then(function([user, role1]){
+    return Role.create({name:'tourist'}).then(function(role2){  //创建角色2
+        return [user, role1, role2];
+    });   
+}).then(function([user, role1, role2]){
+    user.addRoles([role1, role2]);    //用户添加角色1和角色2
+    //分别添加角色
+    //user.addRole(role1);    //用户添加角色1
+    //user.addRole(role2);    //用户添加角色2
 })
-
-
-// sequelize.drop().then(function(){   //删除所有表
-//     console.log("删除所有表成功！");
-// }).then(function(){
-//     return sequelize.sync({force:true}).then(function(){  //创建所有表
-//         console.log("创建数据表成功！");
-//     });
-// }).then(function(){
-//     return Class.create({name:'No.1'}); //创建一条班级数据
-// }).then(function(){
-//     return Student.create({name:'zhang'})   //创建一条学生数据
-// }).then(function(stu){
-//     return stu.setClass(1); //设置学生外键
-// }).then(function(stu){
-//     console.log(JSON.stringify(stu));   //输出最终学生的数据
-// });
 
 
 
