e40127b5da400c44eb9e4dbb9e09e0caf8995698
zhangliang <914342085@qq.com>
2019-04-23 11:43:02 +0800

    javascript webpack增加导入css和图片样例

--- javascript/Webpack/webpack-asset-demo/src/index.js
+++ javascript/Webpack/webpack-asset-demo/src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
-import './style.css'
+import './style.css';
+import Icon from './icon.png';
 
 function component() {
   let element = document.createElement('div');
@@ -7,6 +8,12 @@ function component() {
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  //增加一个icon图片
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
--- javascript/Webpack/webpack-asset-demo/webpack.config.js
+++ javascript/Webpack/webpack-asset-demo/webpack.config.js
@@ -8,12 +8,19 @@ module.exports = {
   },
   module: {
     rules: [{
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        'css-loader'
-      ]
-    }]
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
   },
   mode: 'development'
 };
